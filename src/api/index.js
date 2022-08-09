9a7c6a90fc6a7cd9593bc0804e274bbeebea8923
Melquisedec <melquis772@gmail.com>
2022-08-09 19:52:48 -0400

    updated

--- src/api/index.js
+++ src/api/index.js
@@ -5,10 +5,6 @@ export const getPlacesData = async (type, sw, ne) => {
 
     const options = {
         params: {
-            /* bl_latitude: '11.847676',
-            tr_latitude: '12.838442',
-            bl_longitude: '109.095887',
-            tr_longitude: '109.149359', */
             bl_latitude: sw.lat,
             tr_latitude: ne.lat,
             bl_longitude: sw.lng,
